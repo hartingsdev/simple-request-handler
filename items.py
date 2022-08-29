761c4d8be457e26fc8e23933e81d5bbc24d487d5
Robert Hartings <robert@hartings.dev>
2022-08-29 11:12:29 +0200

    Update GET & Saving Time
    Added time to redis commits for better understanding when a request was received.
    Had to save the data with json string to avoid hash set in redis.
    
    Changed retrieving of items, because it was confusing. Items get inserted in front and the parameters were used to cut of the head.

--- items.py
+++ items.py
@@ -2,15 +2,24 @@ from fastapi import APIRouter, Depends, Response
 import schemas
 from security import get_current_active_user
 from database import redis_db
+from datetime import datetime, timezone
+import json
 
 router = APIRouter(prefix="/i", tags=["items"])
 
 
 @router.get("/", response_model=list)
-def get_all_items_of_user(start: int = 0, end: int = -1,
-                          current_user: schemas.User = Depends(
+# def get_all_items_of_user(start: int = 0, end: int = -1,
+#                           current_user: schemas.User = Depends(
+#                               get_current_active_user)):
+def get_all_items_of_user(current_user: schemas.User = Depends(
                               get_current_active_user)):
-    return redis_db.lrange(current_user.uuid, start, end)
+    result = []
+    # for elem in redis_db.lrange(current_user.uuid, start, end):
+    for elem in redis_db.lrange(current_user.uuid, 0, -1):
+        result.append(json.loads(elem))
+
+    return result
 
 
 @router.delete("/", status_code=204)
@@ -22,11 +31,17 @@ def del_all_items_of_user(current_user: schemas.User =
 
 @router.post("/{uuid}", status_code=204)
 def add_item_via_post(uuid: str, content: schemas.ItemCreate):
-    redis_db.lpush(uuid, content.content)
+    redis_db.lpush(uuid, json.dumps({
+        'content': content.content,
+        'date': datetime.now(timezone.utc).timestamp() * 1000,
+    }))
     return Response(status_code=204)
 
 
 @router.get("/{uuid}/{content}", status_code=204)
 def add_item_via_get(uuid: str, content: str):
-    redis_db.lpush(uuid, content)
+    redis_db.lpush(uuid, json.dumps({
+        'content': content.content,
+        'date': datetime.now(timezone.utc).timestamp() * 1000,
+    }))
     return Response(status_code=204)
